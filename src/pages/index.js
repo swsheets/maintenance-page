ee9603dafd52c28c97cc056ece000efb4d07074c
Nicholas Duffy <duffn@outlook.com>
2020-01-16 16:00:57 -0700

    Update date and time

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,17 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>SWSheets is currently undergoing maintenance.</h1>
     <p>
-      We expect to be complete by{" "}
+      Maintenance began at{" "}
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://www.worldtimebuddy.com/?pl=1&amp;lid=7&amp;h=7&amp;date=1/18/2020%7C3"
+        href="https://www.worldtimebuddy.com/?pl=1&amp;lid=7&amp;h=7&amp;date=1/22/2020%7C3"
       >
-        01/18/2020 8:00AM MST
-      </a>
-      .
+        01/22/2020 8:00PM MST
+      </a>{" "}
+      and we hope to be complete soon.
     </p>
+    <p>May the Force be with us.</p>
     <hr></hr>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
   </Layout>
